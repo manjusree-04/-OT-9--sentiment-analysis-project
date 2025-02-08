b7ddb419635e2310ddf6a4c3a89744cd8943e756
Vidya sree Geega <vidyasreegeega@gmail.com>
2025-02-08 12:09:19 +0530

    Day2

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask,render_template
-app=Flask(__name__)
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
 @app.route("/")
 def Home():
-    return render_template("Index.html")
-
-if __name__=="__main__":
-    app.run()    
+    return render_template("index.html")
+@app.route("/predict",methods=["GET","POST"])
+def predict():
+    if request.method == "POST":
+        msg = request.form.get("message")
+        print(msg)
+    else:
+        return render_template("predict.html")
+    
+if __name__ =="__main__":
+    app.run(host="0.0.0.0",port=5050)
